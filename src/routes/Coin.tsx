bfc89d1a433ad0ce5437f79b1c2015e741edc100
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2023-02-13 11:03:16 +0100

    IS-17754  Spostare rank

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,17 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
 
 import "./coin.css";
 
 export const Coin = () => {
   const params = useParams();
-  
+
   const [coin, setCoin] = useState<any>({});
   const url = "https://api.coingecko.com/api/v3/coins/" + params.coinId;
   useEffect(() => {
-    axios.get(url)
+    axios
+      .get(url)
       .then((response) => {
         setCoin(response.data);
       })
@@ -21,12 +22,15 @@ export const Coin = () => {
     <div>
       <div className="coin-container">
         <div className="content">
-          <h1>{coin.name}</h1>
+          <div className="rank-content">
+            <div className="rank">
+              <span className="rank-btn">Rank # {coin.market_cap_rank}</span>
+            </div>
+          </div>
+
+          <h1 className="title">{coin.name}</h1>
         </div>
         <div className="content">
-          <div className="rank">
-            <span className="rank-btn">Rank # {coin.market_cap_rank}</span>
-          </div>
           <div className="info">
             <div className="coin-heading">
               {coin.image ? (
@@ -34,10 +38,11 @@ export const Coin = () => {
               ) : null}
               <p>{coin.name}</p>
               {coin.symbol ? <p>{coin.symbol.toUpperCase()}/EUR</p> : null}
-              
             </div>
             <div className="coin-price">
-                {coin.market_data?.current_price ? <h1>{coin.market_data.current_price.eur.toLocaleString()} €</h1>: null}
+              {coin.market_data?.current_price ? (
+                <h1>{coin.market_data.current_price.eur.toLocaleString()} €</h1>
+              ) : null}
             </div>
           </div>
         </div>
@@ -56,28 +61,64 @@ export const Coin = () => {
             <tbody>
               <tr>
                 <td>
-                  { coin.market_data?.price_change_percentage_1h_in_currency ? 
-                  <p>{coin.market_data.price_change_percentage_1h_in_currency?.eur.toFixed(1)}%</p> : null}
+                  {coin.market_data?.price_change_percentage_1h_in_currency ? (
+                    <p>
+                      {coin.market_data.price_change_percentage_1h_in_currency?.eur.toFixed(
+                        1
+                      )}
+                      %
+                    </p>
+                  ) : null}
                 </td>
                 <td>
-                  {coin.market_data?.price_change_percentage_24h_in_currency ? 
-                  <p>{coin.market_data.price_change_percentage_24h_in_currency?.eur.toFixed(1)}%</p> : null}
+                  {coin.market_data?.price_change_percentage_24h_in_currency ? (
+                    <p>
+                      {coin.market_data.price_change_percentage_24h_in_currency?.eur.toFixed(
+                        1
+                      )}
+                      %
+                    </p>
+                  ) : null}
                 </td>
                 <td>
-                  {coin.market_data?.price_change_percentage_7d_in_currency ? 
-                  <p>{coin.market_data.price_change_percentage_7d_in_currency?.eur.toFixed(1)}%</p> : null}
+                  {coin.market_data?.price_change_percentage_7d_in_currency ? (
+                    <p>
+                      {coin.market_data.price_change_percentage_7d_in_currency?.eur.toFixed(
+                        1
+                      )}
+                      %
+                    </p>
+                  ) : null}
                 </td>
                 <td>
-                  {coin.market_data?.price_change_percentage_14d_in_currency ? 
-                  <p>{coin.market_data.price_change_percentage_14d_in_currency?.eur.toFixed(1)}%</p> : null}
+                  {coin.market_data?.price_change_percentage_14d_in_currency ? (
+                    <p>
+                      {coin.market_data.price_change_percentage_14d_in_currency?.eur.toFixed(
+                        1
+                      )}
+                      %
+                    </p>
+                  ) : null}
                 </td>
                 <td>
-                  {coin.market_data?.price_change_percentage_30d_in_currency ? 
-                  <p>{coin.market_data.price_change_percentage_30d_in_currency?.eur.toFixed(1)}%</p> : null}
+                  {coin.market_data?.price_change_percentage_30d_in_currency ? (
+                    <p>
+                      {coin.market_data.price_change_percentage_30d_in_currency?.eur.toFixed(
+                        1
+                      )}
+                      %
+                    </p>
+                  ) : null}
                 </td>
                 <td>
-                  {coin.market_data?.price_change_percentage_1y_in_currency ? 
-                  <p>{coin.market_data.price_change_percentage_1y_in_currency?.eur.toFixed(1)}%</p> : null}
+                  {coin.market_data?.price_change_percentage_1y_in_currency ? (
+                    <p>
+                      {coin.market_data.price_change_percentage_1y_in_currency?.eur.toFixed(
+                        1
+                      )}
+                      %
+                    </p>
+                  ) : null}
                 </td>
               </tr>
             </tbody>
@@ -88,32 +129,46 @@ export const Coin = () => {
             <div className="left">
               <div className="row">
                 <h4>24 Hour Low</h4>
-                {coin.market_data?.low_24h ? <p>{coin.market_data.low_24h.eur.toLocaleString()}€</p> : null}
+                {coin.market_data?.low_24h ? (
+                  <p>{coin.market_data.low_24h.eur.toLocaleString()}€</p>
+                ) : null}
               </div>
               <div className="row">
                 <h4>24 Hour High</h4>
-                {coin.market_data?.high_24h ? <p>{coin.market_data.high_24h.eur.toLocaleString()}€</p> : null}
+                {coin.market_data?.high_24h ? (
+                  <p>{coin.market_data.high_24h.eur.toLocaleString()}€</p>
+                ) : null}
               </div>
             </div>
             <div className="right">
               <div className="row">
                 <h4>Market Cap</h4>
-                {coin.market_data?.market_cap ? <p>{coin.market_data.market_cap.eur.toLocaleString()}€</p> : null}
+                {coin.market_data?.market_cap ? (
+                  <p>{coin.market_data.market_cap.eur.toLocaleString()}€</p>
+                ) : null}
               </div>
               <div className="row">
                 <h4>Circulating Supply</h4>
-                <p>{ coin.market_data ? coin.market_data.circulating_supply: null}</p>
+                <p>
+                  {coin.market_data
+                    ? coin.market_data.circulating_supply
+                    : null}
+                </p>
               </div>
             </div>
           </div>
         </div>
         <div className="content">
-            <div className="about">
-                <h3>About</h3>
-                <p dangerouslySetInnerHTML={{
-                    __html: DOMPurify.sanitize(coin.description ? coin.description.en: '')
-                }}></p>
-            </div>
+          <div className="about">
+            <h3>About</h3>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  coin.description ? coin.description.en : ""
+                ),
+              }}
+            ></p>
+          </div>
         </div>
       </div>
     </div>
