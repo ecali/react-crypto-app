a6d134cce7132a73aa9991328219f9cee4a4abb4
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2023-02-13 11:13:50 +0100

    IS-12113 Change name IS-53272 favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import {FaCoins} from 'react-icons/fa';
+import {MdQueryStats} from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
@@ -6,8 +6,8 @@ export const Navbar = () => {
     return(
         <Link to='/'>
             <div className="navbar">
-                <FaCoins className='icon' />
-                <h1>Coin <span className='purple'>Search</span></h1>
+                <MdQueryStats className='icon' />
+                <h1>Crypto <span className='purple'>Search Bay</span></h1>
             </div>
         </Link>
     );
